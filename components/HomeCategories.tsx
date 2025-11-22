f868510b761cea0899c9268eb4b2fb18d9da7d3f
Kenzieeor <ajisatrio068@gmail.com>
2025-11-22 22:39:21 +0700

    meme

--- components/HomeCategories.tsx
+++ components/HomeCategories.tsx
@@ -16,7 +16,7 @@ const HomeCategories = ({ categories }: { categories: Category[] }) => {
             className="bg-shop_light_bg p-5 flex items-center gap-3 group rounded-lg"
           >
             {category?.image && (
-              <div className="overflow-hidden border border-shop_orange/30 hover:border-shop_orange rounded-md hoverEffect w-20 h-20 p-1">
+              <div className="overflow-hidden border border-shop_orange/30 hover:border-shop_orange hoverEffect w-20 h-20 p-1">
                 <Link href={`/category/${category?.slug?.current}`}>
                   <Image
                     src={urlFor(category?.image).url()}
