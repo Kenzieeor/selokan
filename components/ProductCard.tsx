2eae600396203851d36cc116dbfff95e08850c89
Kenzieeor <ajisatrio068@gmail.com>
2025-11-22 13:59:37 +0700

    meme

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,12 +1,14 @@
 import { Product } from "@/sanity.types";
 import { urlFor } from "@/sanity/lib/image";
+import { Flame } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
-    <div>
-      <div>
+    <div className="text-sm border-[1px] border-dark_blue/20 rounded-lg bg-white group">
+      <div className="relative group overflow-hidden bg-shop_light_bg rounded-t-lg">
         {product?.images && (
           <Image
             src={urlFor(product?.images[0]).url()}
@@ -16,7 +18,33 @@ const ProductCard = ({ product }: { product: Product }) => {
             height={500}
           />
         )}
+
+        {product?.status === "sale" && (
+          <p
+            className="absolute top-2 left-2 z-10 p-2 text-xs border border-darkColor/50 
+          px-2 rounded-full group-hover:border-shop_light_green
+        group-hover:text-shop_light_green hoverEffect"
+          >
+            Sale!
+          </p>
+        )}
+
+        {product?.status === "hot" && (
+          <Link
+            href={"/deal"}
+            className="absolute top-2 left-2 z-10 p-2 text-xs border border-shop_orange/50 
+          px-2 rounded-full group-hover:border-shop_orange
+        group-hover:text-shop_dark_green hoverEffect"
+          >
+            <Flame
+              size={18}
+              fill="#fb6c08"
+              className="text-shop_orange/50 group-hover:text-shop_orange hoverEffect"
+            />
+          </Link>
+        )}
       </div>
+      <div className="p-3">Product Details</div>
     </div>
   );
 };
--- components/ProductGrid.tsx
+++ components/ProductGrid.tsx
@@ -47,7 +47,7 @@ const ProductGrid = () => {
 
       {loading ? (
         <div className="flex flex-col items-center justify-center py-10 min-h-80 gap-4 bg-gray-100 w-full mt-10">
-          <div className="space-x-2 flex items-center text-blue-600">
+          <div className="space-x-2 flex items-center text-black">
             <Loader2 className="w-5 h-5 animate-spin" />
             <span>Product is loading...</span>
           </div>
@@ -56,7 +56,12 @@ const ProductGrid = () => {
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2.5 mt-10">
           {products?.map((product) => (
             <AnimatePresence key={product?._id}>
-              <motion.div>
+              <motion.div
+                layout
+                initial={{ opacity: 0.2 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0.2 }}
+              >
                 <ProductCard product={product} />
               </motion.div>
             </AnimatePresence>
